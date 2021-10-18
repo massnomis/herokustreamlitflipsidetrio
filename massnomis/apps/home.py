d06786d7a2adf4677289c002c4c01a5f97a40314
massnomis <massnomis@gmail.com>
2021-10-18 15:07:46 -0400

    Add files via upload

---
+++ massnomis/apps/home.py
@@ -0,0 +1,11 @@
+import plotly
+import streamlit as st
+
+import plotly.express as px
+import statsmodels.api as sm
+import pandas as pd
+import json
+def app():
+
+    #st.set_page_config(layout="wide")
+    st.title("YEET")
